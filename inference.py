096259bc388e65997c9b4ba0afe26f4405b4b80f
tmaximun916 <69234520+tmaximun916@users.noreply.github.com>
2021-12-31 18:08:45 +0800

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+import training
+
+img_height = 180
+img_width = 180
+
+#load model
+model = keras.models.load_model("my_model")
+
+#predict with new data
+testimg_path = "D:\z. Personal Stuff\School STUFFs\Poly\MLAI/test_redherring2.png"
+
+img = tf.keras.utils.load_img(testimg_path, target_size=(img_height, img_width))
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+decimal_score = 100 * np.max(score)
+
+if decimal_score < 90:
+  print("This image most likely belongs to {} with a {:.2f} percent confidence.".format("unknown object", decimal_score))
+else:
+  print(
+      "This image most likely belongs to {} with a {:.2f} percent confidence."
+      .format(training.class_names[np.argmax(score)], decimal_score)
+  )
---
+++ training.py
@@ -0,0 +1,144 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+import pathlib
+data_dir = "D:\z. Personal Stuff\School STUFFs\Poly\MLAI\OwnModel\img"
+data_dir = pathlib.Path(data_dir)
+image_count = len(list(data_dir.glob('*/*.jpg'))) # .glob to look inside subtree
+print(image_count) # print out no. of images
+
+# create a dataset
+batch_size = 32
+img_height = 180
+img_width = 180
+
+#use a validation split when developing model. 80% of the images for training, and 20% for validation.
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2, 
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+
+for image_batch, labels_batch in train_ds:
+  print(image_batch.shape)
+  print(labels_batch.shape)
+  break
+
+#Configure the dataset for performance
+#buffered prefetching so you can yield data from disk without having I/O become blocking.
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE) #keeps the images in memory after they're loaded off disk during the first epoch
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+#Standardize the data
+normalization_layer = layers.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
+
+#code to augment data using keras api(from tensorflow website)
+data_augmentation = keras.Sequential(
+  [
+    layers.RandomFlip("horizontal",
+                      input_shape=(img_height,
+                                  img_width,
+                                  3)),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+  ]
+)
+
+#Create the model
+num_classes = len(class_names)
+
+model = Sequential([
+  data_augmentation,
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2), #to reduce overfitting, introduce dropout regularization to the network.
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+#Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+#train the model
+epochs=15
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+#save the model in SavedModel format
+model.save("my_model")
+
+#visualise
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
